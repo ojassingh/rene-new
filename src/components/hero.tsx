7916600aff66aff1aafa91e9938af1ad5e6c11be
Ojas Singh <ojas.singh02@gmail.com>
2025-01-30 16:01:41 -0500

    Add shad/cn and upgrade tailwind to v4

---
+++ src/components/hero.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Hero: FC = () => {
+  return (
+    <section id="home" className="p-20">
+      <h1 className="font-(--heading) text-5xl">Renesh</h1>
+    </section>
+  );
+};
+
+export default Hero;
